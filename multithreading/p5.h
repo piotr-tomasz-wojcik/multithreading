01449b244b09dcaeed750bcf7595eab7aad8b0f7
Piotr Wójcik <piotr.2.wojcik@nokia.com>
2018-09-20 16:50:48 +0200

    Example with listing filesystem

--- multithreading/p5.h
+++ multithreading/p5.h
@@ -3,6 +3,9 @@
 #include <thread>
 #include <vector>
 #include <future>
+#include <experimental/filesystem>
+
+using namespace std::experimental::filesystem::v1;
 
 namespace p5 
 {
@@ -46,4 +49,81 @@ namespace p5
         }
     }
 
+    using strings = std::vector<std::string>;
+
+    std::string wide2std(std::wstring& ws)
+    {
+        const std::locale locale("");
+        typedef std::codecvt<wchar_t, char, std::mbstate_t> converter_type;
+        const converter_type& converter = std::use_facet<converter_type>(locale);
+        std::vector<char> to(ws.length() * converter.max_length());
+        std::mbstate_t state;
+        const wchar_t* from_next;
+        char* to_next;
+        const converter_type::result result = converter.out(state, ws.data(), ws.data() + ws.length(), from_next, &to[0], &to[0] + to.size(), to_next);
+        if (result == converter_type::ok or result == converter_type::noconv) {
+            const std::string s(&to[0], to_next);
+            return s;
+        }
+        return "";
+    }
+
+    strings listDirectory(std::string && dir)
+    {
+        strings listing;
+        std::string dirStr("\n> ");
+        dirStr += dir;
+        dirStr += ":\n";
+        listing.push_back(dirStr);
+
+        std::vector<std::future<strings>> futures;
+
+        std::cout << "Dir: " << dirStr << std::endl;
+
+        for (directory_entry entry : directory_iterator(dir))
+        {
+            if (is_directory(entry))
+            {
+                std::wstring wpath = entry.path().native();
+
+                auto future = std::async(std::launch::async, 
+                    &listDirectory, 
+                    wide2std(wpath));
+
+                futures.push_back(std::move(future));
+            }
+            else
+            {
+                std::wstring wpath = entry.path().filename().native();
+                listing.push_back(wide2std(wpath));
+            }
+        }
+
+        for (auto& future : futures)
+        {
+            strings list = future.get();
+            std::copy(list.begin(), list.end(), std::back_inserter(listing));
+        }
+        
+        return listing;
+    }
+
+    void executeListDirectory(std::string root)
+    {
+        auto future = std::async(std::launch::async, &listDirectory, root);
+        try
+        {
+            strings listing = future.get();
+            for (auto& filename : listing)
+                std::cout << filename << std::endl;
+        }
+        catch (std::exception & e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+        catch (...)
+        {
+            std::cout << "Unknown exception\n";
+        }
+    }
 }
